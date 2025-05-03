408b9bd28029fa65326cef5a516c34cbbc84b1d9
Om Singh <omsingh32123@gmail.com>
2025-05-03 14:38:01 +0530

    removed eslint type check

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,9 @@ import createMDX from '@next/mdx'
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // Optionally, add any other Next.js config below
 }
 
